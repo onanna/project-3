4ffe3104339e473d922a462a4a77ae1e9b1d4606
Cartagena Anthony <anthonym.cartagena@gmail.com>
2019-02-05 16:17:04 -0500

    Added root directory for project. kept some reference code in some files for us to use

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  return (
+    <div><h1>Hello Team!</h1></div>
+  );
+}
+
+export default App;
---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+// const bookRoutes = require("./books");
+
+//this is where we tell the code which router to use for which routes
+
+// Book routes
+// router.use("/books", bookRoutes);
+
+module.exports = router;
