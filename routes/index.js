41c100f082c18cd093dd72e4e4affb36897d0a22
JayBee <jemallbrown@gmail.com>
2019-02-21 21:13:01 -0500

    copy and paste to fix branch

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const userRoutes = require("./user")
+
+// // API Routes
+router.use("/api", apiRoutes);
+router.use("/user",userRoutes);
+
+router.get("/",(req,res)=>{
+  console.log(res)
+})
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
