fb2253c5fc699c312eeea1d5082931da7387953b
Cartagena Anthony <anthonym.cartagena@gmail.com>
2019-02-26 16:45:17 -0500

    figured out how it can work without passport. set App.js as stateful componennt

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Sidenav from "./components/sidenav/sidenav";
 import "./App.css";
-
 import AllCourses from "./pages/courseDirectory"
 import NewCourse from "./pages/newCourse/newCourse"
 import InstDirectory from "./pages/instructorDirectory"
@@ -11,35 +10,59 @@ import Course from "./pages/savedCourse/savedCourse";
 import NewInstructor from "./components/newInstructorForm/newInstructorForm";
 import Login from "./pages/login";
 import NewLoginform from "./components/newLoginForm/newLoginForm";
+import API from "./utils/API";
 
-function App() {
-  return (
-      <Router>
-        <div>
-          {/* <Sidenav /> */}
-          <Switch>
-            <Route exact path="/" component={AllCourses} />
-            <Route path="/login" component={Login} />
-            <Route exact path="/user/new" component={NewLoginform}/>
-            
-            {/* <Route exact path="/api/students" component={StuDirectory} /> */}
-            {/* <Route exact path="/api/instructors" component={InstDirectory} /> */}
-            {/* <Route exact path="/savedcourse" component={Course} />   */}
+class App extends Component{
+  
+  state={
+    token:'',
+    isLoggedIn:false
+  }
+  
+  componentDidMount=()=>{
+    if(localStorage.getItem("course-creator-token")){
+      let token=localStorage.getItem("course-creator-token");
 
-            <Route exact path="/students/all" component={StuDirectory} />
-            {/* <Route exact path="/students/:id" component={StuDirectory} /> */}
-            <Route exact path="/instructors/all" component={InstDirectory} />            
-            {/* <Route exact path="/instructors/:id" component={InstDirectory} /> */}
-            {/* <Route exact path="/courses" component={Course} /> */}
-            <Route exact path="/courses/detail/:id" component={Course} />
-            {/* <Route exact path="/courses/detail/attendance/:date" component={Attendance} /> */}
-            
-            <Route exact path="/newcourse" component={NewCourse}  />
-            <Route exact path="/newinstructorform" component={NewInstructor}  />
-          </Switch>
-        </div>
-      </Router>
-  );
+      //check database for token
+
+      this.setState({token: token})
+    }
+  }
+
+  render(){
+
+    if(!this.state.token){
+      return <Login />
+    }else{
+      return (
+          <Router>
+            <div>
+              {/* <Sidenav /> */}
+              <Switch>
+                <Route exact path="/" component={AllCourses} />
+                <Route path="/login" component={Login} />
+                <Route exact path="/user/new" component={NewLoginform}/>
+                
+                {/* <Route exact path="/api/students" component={StuDirectory} /> */}
+                {/* <Route exact path="/api/instructors" component={InstDirectory} /> */}
+                {/* <Route exact path="/savedcourse" component={Course} />   */}
+
+                <Route exact path="/students/all" component={StuDirectory} />
+                {/* <Route exact path="/students/:id" component={StuDirectory} /> */}
+                <Route exact path="/instructors/all" component={InstDirectory} />            
+                {/* <Route exact path="/instructors/:id" component={InstDirectory} /> */}
+                {/* <Route exact path="/courses" component={Course} /> */}
+                <Route exact path="/courses/detail/:id" component={Course} />
+                {/* <Route exact path="/courses/detail/attendance/:date" component={Attendance} /> */}
+                
+                <Route exact path="/newcourse" component={NewCourse}  />
+                <Route exact path="/newinstructorform" component={NewInstructor}  />
+              </Switch>
+            </div>
+          </Router>
+      );
+    }
+  }
 }
 
 export default App;
