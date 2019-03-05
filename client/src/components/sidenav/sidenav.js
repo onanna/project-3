0aebc111d31155e276e4293591b40c12441c2885
Cartagena Anthony <anthonym.cartagena@gmail.com>
2019-03-05 12:49:51 -0500

    fixed sidenav trigger/display issue. fixed pageContainer width/margin. courses now display sorted by recently added

--- client/src/components/sidenav/sidenav.js
+++ client/src/components/sidenav/sidenav.js
@@ -6,7 +6,7 @@ function sidenav(props){
 
     return(
         <div>
-            <ul id="slide-out" className="sidenav sidenav-fixed">
+            <ul id="slide-out-sidenav" className="sidenav sidenav-fixed">
                 <li><div className="user-view">
                     {/* <div class="background">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNADC7o5R7BPr3GB4-xQSeTxm1VE2IW7c2nCtz0JPlj6Tg4edS" />>
@@ -17,14 +17,13 @@ function sidenav(props){
                     {/* <a href={`user/profile`}><span class="white-text email">jdandturk@gmail.com</span></a> */}
                     </div></li>
                 {/* <li><div className='divider'></div></li> */}
-                <li className="firstItem sidenav-close"><a href="/">Go Back Home</a></li>
-                <li className="sidenav-close"><a href="/students/all">View All Students</a></li>
-                <li className="sidenav-close"><a href="/instructors/all">View All Instructors</a></li>
-                <li className="sidenav-close"><a href="/newcourse">Add New Course</a></li>
+                <li className="firstItem sidenav-close"><a href="/">Home</a></li>
+                <li className="sidenav-close"><a href="/students/all">Students</a></li>
+                <li className="sidenav-close"><a href="/instructors/all">Instructors</a></li>
+                <li className="sidenav-close"><a href="/newcourse">Add Course</a></li>
                 {/* <li className="sidenav-close"><a href="/savedCourse">View Courses</a></li> */}
                 <li className="sidenav-close"> <Logout deleteToken={props.deleteToken}/></li>
             </ul>
-            <a data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         </div>
     )
 }
